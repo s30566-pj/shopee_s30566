82c289e74e3477673cdbe857f7c9edbbe0ac4cbc
s30566 <s30566@pjwstk.edu.pl>
2025-01-29 17:15:26 +0100

    First commit

---
+++ src/main/java/pj/s30566/shopee_s30566/ShopeeS30566Application.java
@@ -0,0 +1,13 @@
+package pj.s30566.shopee_s30566;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeeS30566Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopeeS30566Application.class, args);
+	}
+
+}
---
+++ src/test/java/pj/s30566/shopee_s30566/ShopeeS30566ApplicationTests.java
@@ -0,0 +1,13 @@
+package pj.s30566.shopee_s30566;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeeS30566ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
